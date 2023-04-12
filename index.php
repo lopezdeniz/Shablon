0ea4e4840c97affb2c344f4be75cfedaf97df0e5
lopez <lopesdenis@mail.ru>
2023-04-12 23:02:31 +0300

    site map and services xml

--- index.php
+++ index.php
@@ -18,11 +18,33 @@
   <link href="css/style.css" rel="stylesheet">
   <link href="css/style2.css" rel="stylesheet">
   <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
-  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
-  <link rel="manifest" href="site.webmanifest">
 
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
+  <link rel="manifest" href="site.webmanifest">
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(93189644, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/93189644" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 
 </head>
 
@@ -177,14 +199,16 @@
               </div>
               <br>
               <br>
-              <p class="fs-5"><a class="text-black text-decoration-none" href=”tel:+79854487143”>+7(985)448-71-43</a>
-              </p>
+              <div class="fs-5"><div class="text-black text-decoration-none">+7(985)448-71-43</div>
+              
+            
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-
+    
 
 
 
@@ -600,7 +624,10 @@
               </div>
               <br>
               <br>
-              <p class="fs-5"><a class="text-black text-decoration-none" href=”tel:+79854487143”>+7(985)448-71-43</a>
+              <div class="fs-5"><div class="text-black text-decoration-none">+7(985)448-71-43</div>
+              
+            
+              </div>
               </p>
             </div>
           </div>
